94c8afac1ad363419e6ddbe37fa604ea1909a7cb
mimaruya <mimaruya@yahoo-corp.jp>
2020-05-10 15:21:00 +0900

    Initial Commit

---
+++ SampleCollections/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SampleCollections
+//
+//  Created by 丸山 三喜也 on 2020/05/10.
+//  Copyright © 2020 丸山 三喜也. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
